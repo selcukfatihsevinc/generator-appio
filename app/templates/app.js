273279057e3c059bedbd5d9ba69cde93ed9ffab3
Selcuk Fatih Sevinc <selcukfatihsevinc@Selcuks-MacBook-Pro.local>
2016-07-12 14:37:24 +0300

    v1.0.2
    * dependency updates
    * default nodejs version update

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,20 +1,2 @@
 var AppIo = require('app.io');
 new AppIo({basedir: __dirname}).run();
-
-/*
-var AppIo = require('app.io');
-new AppIo({
-	basedir: __dirname,
-	verbose: true,
-	boot: 'mailer|forward',
-	resize: true,
-	core: ['mongo', 'redis', 'cache'],
-	external: {
-		boot: 'i18n|gitversion',
-		model: [],
-		middle: [],
-		lib: [],
-		route: []
-	}
-}).run();
-*/
